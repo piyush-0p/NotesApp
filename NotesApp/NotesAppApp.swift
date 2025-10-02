4277029bdaff7a7e941d854b5af53f98bd3d725c
piyush-0p <145971445+piyush-0p@users.noreply.github.com>
2025-10-02 23:27:35 +0530

    Initial Commit

---
+++ NotesApp/NotesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesAppApp.swift
+//  NotesApp
+//
+//  Created by Piyush on 02/10/25.
+//
+
+import SwiftUI
+
+@main
+struct NotesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
